c2e0bf7ec9a51cd5d02593b1843eda2221f10457
Yukari Sakurai <neko3genki@gmail.com>
2015-07-27 00:01:45 +0900

    replaced app icons

--- app/src/main/java/com/sakurafish/parrot/callconfirm/activity/MainActivity.java
+++ app/src/main/java/com/sakurafish/parrot/callconfirm/activity/MainActivity.java
@@ -1,17 +1,23 @@
 package com.sakurafish.parrot.callconfirm.activity;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.sakurafish.parrot.callconfirm.R;
 import com.sakurafish.parrot.callconfirm.fragment.MainFragment;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
+    private ListView mDrawerList;
     private Fragment mContent;
 
     public static Intent createIntent(Context context, Class clazz) {
@@ -24,6 +30,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        initLayout();
 
         if (savedInstanceState == null) {
             mContent = MainFragment.getInstance();
@@ -38,15 +48,38 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+    private void init() {
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+    }
+
+    private void initLayout() {
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+        // TODO
+//        mDrawerList.setAdapter(new NavExpandableAdapter(mContext));
+    }
+
     @Override
     public void onSaveInstanceState(final Bundle outState) {
         getFragmentManager().putFragment(outState, "mContent", mContent);
         super.onSaveInstanceState(outState);
     }
 
+    private void finalizeLayout() {
+        mDrawerList = null;
+    }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
+        finalizeLayout();
         mContent = null;
     }
 }
