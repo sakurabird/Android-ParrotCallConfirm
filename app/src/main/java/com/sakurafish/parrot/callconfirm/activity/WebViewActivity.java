3e40e8c3f881f91c6e524f39245da0730bf21be3
Yukari Sakurai <neko3genki@gmail.com>
2018-04-02 20:32:37 +0900

    Add adView to WebViewActivity

--- app/src/main/java/com/sakurafish/parrot/callconfirm/activity/WebViewActivity.java
+++ app/src/main/java/com/sakurafish/parrot/callconfirm/activity/WebViewActivity.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.databinding.DataBindingUtil;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -11,14 +12,19 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 
+import com.google.android.gms.ads.AdRequest;
 import com.sakurafish.parrot.callconfirm.R;
+import com.sakurafish.parrot.callconfirm.databinding.ActivityMainBinding;
 import com.sakurafish.parrot.callconfirm.fragment.WebFragment;
+import com.sakurafish.parrot.callconfirm.utils.AdsHelper;
 
 import static com.sakurafish.parrot.callconfirm.fragment.BaseWebFragment.EXTRA_TITLE;
 import static com.sakurafish.parrot.callconfirm.fragment.BaseWebFragment.EXTRA_URL;
 
 
 public class WebViewActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     private Fragment mContent;
     private String mUrl;
@@ -42,7 +48,7 @@ public class WebViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         initialize(savedInstanceState);
     }
@@ -68,6 +74,10 @@ public class WebViewActivity extends AppCompatActivity {
                     .replace(R.id.content, mContent)
                     .addToBackStack("WebFragment").commit();
         }
+
+        // show AD banner
+        AdRequest adRequest = new AdsHelper(this).getAdRequest();
+        binding.adView.loadAd(adRequest);
     }
 
     @Override
