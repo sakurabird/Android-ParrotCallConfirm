99dbf4a9aae95ade569b417fa1e353b5d10e0f70
Yukari Sakurai <neko3genki@gmail.com>
2018-07-11 04:48:01 +0900

    Add BootReceiver in order to clear flag

--- app/src/main/java/com/sakurafish/parrot/callconfirm/receiver/BootReceiver.java
+++ app/src/main/java/com/sakurafish/parrot/callconfirm/receiver/BootReceiver.java
@@ -1,4 +1,4 @@
-package com.sakurafish.parrot.callconfirm;
+package com.sakurafish.parrot.callconfirm.receiver;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -7,12 +7,15 @@ import android.content.Intent;
 import com.sakurafish.parrot.callconfirm.Pref.Pref;
 import com.sakurafish.parrot.callconfirm.config.Config;
 
+import static com.sakurafish.parrot.callconfirm.config.Config.PREF_STATE_INVALID_TELNO;
+
 public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, final Intent intent) {
         if (null != intent && "android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
             Pref.setPref(context, Config.PREF_AFTER_CONFIRM, false);
+            Pref.setPref(context, PREF_STATE_INVALID_TELNO, false);
         }
     }
 }
